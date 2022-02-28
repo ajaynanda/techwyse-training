dfb09495cb1e2d5828920b111e4a6442d8409bb9
Ajay nanda <ajaynanda.techintl@gmail.com>
2022-02-28 13:10:07 +0530

    Promise in controller checking

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -14,93 +14,105 @@ const Register = ([
     check('email', "Email is not valid")
         .isEmail()
         .normalizeEmail()
-], (req, res,next) => {
-    Userdb.findOne({ Email: req.body.email }).then(async (user) => {
-        const errors = validationResult(req)
-        if (!errors.isEmpty()) {
-            return res.status(401).json({ errors: errors.array() })
-        }
-        if (user) {
-            console.log("email already exists");
-            return res.status(401).json({
-                error: true,
-                message: "Email already exists",
-            })
-        } else {
-            const hashpassword = await bcrypt.hash(req.body.password, 10)
-            const user = new Userdb({
-                Firstname: req.body.fname,
-                Lastname: req.body.lname,
-                Email: req.body.email,
-                Password: hashpassword,
-                dateofbirth: req.body.dob,
-                Gender: req.body.gender,
-                Proffession: req.body.prof
-            })
-            user.save(user).then(() => {
-                if(user){
-                    req.data = user
-                        next()
-                }
-            }).catch(() => {
+], (req, res) => {
+    return new Promise((resolve,reject)=>{
+        Userdb.findOne({ Email: req.body.email }).then(async (user) => {
+            const errors = validationResult(req)
+            if (!errors.isEmpty()) {
+                return res.status(401).json({ errors: errors.array() })
+            }
+            if (user) {
+                console.log("email already exists");
                 return res.status(401).json({
+                    error: true,
                     message: "Email already exists",
                 })
-            })
-        }
+            } else {
+                const hashpassword = await bcrypt.hash(req.body.password, 10)
+                const user = new Userdb({
+                    Firstname: req.body.fname,
+                    Lastname: req.body.lname,
+                    Email: req.body.email,
+                    Password: hashpassword,
+                    dateofbirth: req.body.dob,
+                    Gender: req.body.gender,
+                    Proffession: req.body.prof
+                })
+                user.save(user).then(() => {
+                    if(user){
+                      return resolve(user)
+                    }
+                }).catch(() => {
+                    return res.status(401).json({
+                        message: "something went wrong while registering",
+                    })
+                })
+            }
+        })
     })
+    
 })
 const Login = ((req, res,next) => {
-    Userdb.findOne({ Email: req.body.email }).then((user) => {
-        if (!user) {
-            res.status(401).json({ Error: true, Message: "no user found" })
-        }
-        if (user) {
-            const password = user.Password
-            bcrypt.compare(req.body.password, password).then((users) => {
-                console.log(users);
-                if (!users) {
-                    res.status(401).json({ Error: true, Message: "password incorrect" })
-                }
-                if (users) {
-                    const data = {
-                        name: user.Firstname,
-                        email: user.Email
+    return new Promise((resolve,reject)=>{
+
+        Userdb.findOne({ Email: req.body.email }).then((user) => {
+            if (!user) {
+                return  res.status(401).json({ Error: true, Message: "Email Incorrect" })
+            }
+            if (user) {
+                const password = user.Password
+                bcrypt.compare(req.body.password, password).then((users) => {
+                    console.log(users);
+                    if (!users) {
+                        return res.status(401).json({ Error: true, Message: "Password incorrect" })
                     }
-                    const token = jwt.sign(data, process.env.key, { expiresIn: '15m' })
-                    res.cookie('access token', token, {
-                        maxAge: 1000 * 60 * 15,
-                        secure: true,
-                        httpOnly: true,
-                        sameSite: 'lax'
-                    })
-                    console.log(req.cookies);
-                req.token = token 
-                req.userid = user.id
-                    next()
-                }
-            }).catch((err) => {
-                console.log(err);
-            })
-        }
+                    if (users) {
+                        const data = {
+                            name: user.Firstname,
+                            email: user.Email
+                        }
+                        const token = jwt.sign(data, process.env.key, { expiresIn: '15m' })
+                        res.cookie('access-token', token, {
+                            maxAge: 1000 * 60 * 15,
+                            secure: true,
+                            httpOnly: true,
+                            sameSite: 'lax'
+                        })
+                        console.log(req.cookies);
+                    req.token = token 
+                    req.user = user
+                        next()
+                    }
+                }).catch((err) => {
+                 return  reject(err);
+                })
+            }
+        })
     })
+   
 })
-const getuserById = ((req, res,next) => {
-    const id = req.params.id
-    Userdb.findById(id).then((user) => {
-        req.user = user
-        next()
-    }).catch((err) => {
-        res.status(404).json({ Error: true, Message: "User not found with the id " + id })
+const getuserById = ((req, res) => {
+    
+    return new Promise((resolve,reject)=>{
+        const id = req.query.id 
+        console.log(id);
+        Userdb.findById(id).then((user) => {
+          return resolve(user)
+        }).catch((err) => {
+            return reject({Error:true,Message:"User not found with the id " + id })
+        })
     })
+  
 })
-const getAllUser = ((req, res,next) => {
-    Userdb.find().then((user) => {
-         req.users = user
-        next()
-    }).catch((err) => {
-        res.send(err)
+const getAllUser = (() => {
+    return new Promise((resolve,reject)=>{
+        Userdb.find().then((user) => {
+           return resolve(user)
+       }).catch((err) => {
+         return  reject(err)
+       })
     })
+    
 })
 const updateUserById = ((req, res,next) => {
     const id = req.params.id
@@ -112,6 +124,10 @@ const updateUserById = ((req, res,next) => {
         Gender: req.body.gender,
         Proffession: req.body.prof
     })
+    if(!req.body){
+      return  res.status(500).json({message:"Enter all fields"})
+    }
+    console.log("update triggeredd in angular");
         Userdb.findByIdAndUpdate(id, user).then((users) => {
             req.name=users.Firstname
             req.users = users
@@ -133,7 +149,8 @@ const Deleteuser=((req,res,next)=>{
 })
 const Logout = ((req,res,next)=>{
     console.log(req.session); 
-     req.session.destroy(function(err){  
+     req.session.destroy(function(err){
+         res.status(200).json({message:"You have logged out"})  
         if(err) console.log(err);  
          else  
         {  
@@ -148,12 +165,10 @@ const ChangePassword =(async(req,res,next)=>{
                 if(!comparepassword){
                   return  res.status(401).json({
                         error:true,
-                        message:"Password not matching"
+                        message:"Current Password is incorrect"
                     })
                 }else{
-                    console.log("done");
                     const hashpassword = await  bcrypt.hash(req.body.npassword,10)
-                    console.log(hashpassword);
                     const password ={
                        Password: hashpassword
                     }
@@ -161,22 +176,21 @@ const ChangePassword =(async(req,res,next)=>{
                         req.user = res
                         next()
                     }).catch(err=>{
-                      return  res.status(401).json("Error updating password")
+                      return  res.status(401).json("Error while Changing the password")
                     })
                 }
-                  
         }).catch(err=>{
-            return  res.status(401).json("Not found the user")
+            return  res.status(401).json({message:"Not found the user"})
         })
 })
 const verifytoken = ((req, res, next) => {
     let authHeader = req.headers.authorization;
     if (authHeader == undefined) {
-        res.status(401).json({ Error: true, Message: "You have already loggedout or no token found" })
+        res.status(401).json({ Error: true, message: "You have already loggedout or no token found" })
     }
     let token = authHeader.split(" ")[1]
     jwt.verify(token, process.env.key, ((err, result) => {
-        if (err) res.status(401).json({ Error: true, Message: "Authentication failed" })
+        if (err) res.status(401).json({ Error: true, message: "Authentication failed" })
         else {
             next()
         }
--- server/router/router.js
+++ server/router/router.js
@@ -1,11 +1,13 @@
 const router = require("express").Router()
-const { Register } = require("../Controller/Usercontroller")
 const Controllers = require('../Controllers/userController')
-router.post('/register',Controllers.Register,((req,res)=>{
-   return res.status(201).json({
-            success: true,
-            message: "You are registered",
-            data:req.data
+router.post('/register',((req,res)=>{
+        Controllers.Register().then((result)=>{
+                return res.status(201).json({
+                        success: true,
+                        message: "You are registered",
+                        data:result
+        })
+ 
 })
 }))
 router.post('/login',Controllers.Login,((req,res)=>{
@@ -13,39 +15,47 @@ router.post('/login',Controllers.Login,((req,res)=>{
                 success: true,
                 Message: "You are logged in",
                 token: req.token,
-                id: req.userid
+                user: req.user
             })
 }))
-router.get('/getuser/:id',Controllers.getuserById,((req,res)=>{
-      return  res.status(200).json({ 
-              success: true, 
-              data:req.user })
+router.get('/getuser:id',((req,res)=>{
+        const id = req.params.id
+        console.log(id);
+        Controllers.getuserById().then((result)=>{
+                return  res.status(200).send({ 
+                        success: true, 
+                        data:result})
+        })
+      
 }))
-router.get('/getalluser',Controllers.getAllUser,((req,res)=>{
-        return res.status(200).json(
-                {success:true,
-                data:req.users})
+router.get('/getalluser',((req,res)=>{
+        Controllers.getAllUser().then((result)=>{
+                return res.status(200).json(
+                        {success:true,
+                        data:result})
+        })
+        
 }))
-router.put('/updateuser/:id',Controllers.updateUserById,((req,res)=>{
+router.put('/updateuser/:id',Controllers.verifytoken,Controllers.updateUserById,((req,res)=>{
        return res.status(200).json({success:true,
-        message:"Updated the document of " +req.name,
+        message:"Updated Sucessfully",
         data:req.users})
 }))
-router.delete('/deleteuser/:id',Controllers.Deleteuser,((req,res)=>{
+router.delete('/deleteuser/:id',Controllers.verifytoken,Controllers.Deleteuser,((req,res)=>{
         return res.status(200).json({
                 success:true,
                 message:"user deleted sucessfully",
                 userDeleted:req.user
                 })
 }))
-router.get('/changepassword/:id',Controllers.ChangePassword,((req,res)=>{
+router.put('/changepassword/:id',Controllers.verifytoken,Controllers.ChangePassword,((req,res)=>{
         res.status(200).json({
                 success:true,
                 message:"Password changed succesfully",
                 user:req.user
         })
 }))
-router.get('/logout',Controllers.Logout,((req,res)=>{
+router.get('/logout',Controllers.verifytoken,Controllers.Logout,((req,res)=>{
         res.status(200).json({
                 success:true,
                 message:'User logged out'
